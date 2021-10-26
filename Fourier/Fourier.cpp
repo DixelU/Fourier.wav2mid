658c4fd3f9a2fe50fa5978668ee20610c32b2538
DixelU <89090823215@yandex.ru>
2021-10-26 17:26:20 +0500

    Better conversion quality

--- Fourier/Fourier.cpp
+++ Fourier/Fourier.cpp
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <cmath>
 #include <fstream>
+#include <complex>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
@@ -14,22 +15,21 @@
 inline double KeyToFreq(double Key) {
 	return 440. * std::pow(2, (Key - 69.) / 12.);
 }
-inline double GetDFTCoeficient(INT16* Samples,DWORD SampleAmount, double TicksPerSample) {
-	double Acc = 0, Num = -2.f*PI*TicksPerSample / SampleAmount;
+inline double GetDFTCoeficient(INT16* Samples, DWORD SampleAmount, double TicksPerSample) {
+	double Num = -2.f * PI * TicksPerSample / SampleAmount;
+	std::complex<double> Acc = 0;
 	for (int t = 0; t < SampleAmount; ++t) {
-		Acc += std::cos(Num*t)*(Samples[t]/32767.f)*2.f;
+		Acc += (std::cos(Num * t) + std::complex<double>(0, 1) * std::sin(Num * t)) * (Samples[t] / 32767.) * 2.;
 	}
-	return Acc;
+	return std::abs(Acc);
 }
-inline void DFT(INT16* Samples, DWORD SampleAmount, std::vector<double>&Hertz, double*& Output, double SamplesPerSecond) {//hertz = ticks/second
+inline void DFT(INT16* Samples, DWORD SampleAmount, std::vector<double>& Hertz, double*& Output, double SamplesPerSecond) {//hertz = ticks/second
 	for (int i = 0; i < Hertz.size(); i++) {
-		Output[i] = abs(GetDFTCoeficient(Samples, SampleAmount, Hertz[i]/SamplesPerSecond))*(i/64.);
+		Output[i] = GetDFTCoeficient(Samples, SampleAmount, Hertz[i] / SamplesPerSecond) * (i / 64.);
 	}
 }
 
-
-
-int main(){
+int main() {
 	system("mode CON: COLS=90 LINES=15");
 	std::cout << "OFD will open soon...\n";
 	OPENFILENAME ofn;       // common dialog box structure
@@ -56,25 +56,25 @@ int main(){
 		Filename.push_back(szFile[i]);
 	}
 
-	std::ifstream in(Filename,std::ios::binary | std::ios::in);
+	std::ifstream in(Filename, std::ios::binary | std::ios::in);
 	std::ofstream out;
 	std::vector<BYTE> Data;
 	std::vector<double> Hertz;
-	std::deque<BYTE> FinalTrack;
+	std::vector<BYTE> FinalTrack;
 	double* Output;
-	INT16 *Samples;
+	INT16* Samples;
 	DWORD NotesPerSecond;
 	DWORD SampleRate;
 	BYTE T;
 	BOOL First = 1;
-	size_t filesize,SampleArraySize;
+	size_t filesize, SampleArraySize;
 	sf::InputSoundFile ISD;
 
 	in.seekg(0, std::ios::end);
 	filesize = in.tellg();
 	in.seekg(0, std::ios::beg);
 
-	Data.reserve(filesize+5);
+	Data.reserve(filesize + 5);
 	while (in.good() && !in.eof()) {
 		Data.push_back(in.get());
 	}
@@ -83,16 +83,15 @@ int main(){
 	if (ISD.openFromMemory(Data.data(), Data.size())) {
 		std::cout << "Type in amount of ticks per second in the output midi.\nIf you don't understand what this means, type in 0. Default value will be assigned\nType in... ";
 		std::cin >> NotesPerSecond;
-		if (NotesPerSecond<1)NotesPerSecond = 64;
+		if (NotesPerSecond < 1)NotesPerSecond = 64;
 
 		Output = new double[128];
 		for (int i = 0; i < 128; i++)
 			Hertz.push_back(KeyToFreq((float)i));
-		SampleRate = ISD.getSampleRate()*ISD.getChannelCount();
-		Samples = new INT16[(SampleArraySize = SampleRate/NotesPerSecond)];
+		SampleRate = ISD.getSampleRate() * ISD.getChannelCount();
+		Samples = new INT16[(SampleArraySize = SampleRate / NotesPerSecond)];
 		//TEMPO = 60000000.;
 		std::cout << "Things are prepaired\n";
-
 
 		FinalTrack.push_back(0);
 		FinalTrack.push_back(0xFF);
@@ -107,7 +106,7 @@ int main(){
 			DFT(Samples, SampleArraySize, Hertz, Output, NotesPerSecond);
 			First = 1;
 			for (int i = 0; i < 128; i++) {
-				T = (Output[i] < 0) ? 0 : (Output[i] > 128) ? 128 : Output[i];
+				T = (Output[i] < 0) ? 0 : (Output[i] > 127) ? 127 : Output[i];
 				if (!T)continue;
 				FinalTrack.push_back(0);
 				FinalTrack.push_back(0x90 | (T >> 4));
@@ -115,7 +114,7 @@ int main(){
 				FinalTrack.push_back(T);
 			}
 			for (int i = 0; i < 128; i++) {
-				T = (Output[i] < 0) ? 0 : (Output[i] > 128) ? 128 : Output[i];
+				T = (Output[i] < 0) ? 0 : (Output[i] > 127) ? 127 : Output[i];
 				if (!T)continue;
 				FinalTrack.push_back(First);
 				FinalTrack.push_back(0x80 | (T >> 4));// *(FinalTrack.rend() + 514)&0xF
@@ -137,13 +136,13 @@ int main(){
 		out.put(0); out.put(0); out.put(0); out.put(6);
 		out.put(0); out.put(1);
 		out.put(0); out.put(1);
-		out.put(NotesPerSecond>>8); out.put(NotesPerSecond&0xFF);//PPQN
+		out.put(NotesPerSecond >> 8); out.put(NotesPerSecond & 0xFF);//PPQN
 		out.put('M'); out.put('T'); out.put('r'); out.put('k');
-		out.put(FinalTrack.size()>>24);
-		out.put((FinalTrack.size() >> 16)&0xFF);
+		out.put(FinalTrack.size() >> 24);
+		out.put((FinalTrack.size() >> 16) & 0xFF);
 		out.put((FinalTrack.size() >> 8) & 0xFF);
 		out.put(FinalTrack.size() & 0xFF);
-		std::copy(FinalTrack.begin(), FinalTrack.end(), std::ostream_iterator<BYTE>(out));
+		out.write((const char*)FinalTrack.data(), FinalTrack.size());
 		out.close();
 	}
 
